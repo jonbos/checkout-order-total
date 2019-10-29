af3fbd6319cabddeb8b1baadd63ae182e4ce17f4
Jon Bos <bos.jon@gmail.com>
2019-10-29 17:47:36 -0500

    Adds scanning functionality and tests. Scans ByEach and ByWeight items.

--- src/transaction.py
+++ src/transaction.py
@@ -1,7 +1,24 @@
+from src.item import ByEachItem, ByWeightItem
+
 class Transaction:
     def __init__(self, price_db=None):
         if price_db is None:
             price_db = {}
-        self.total = 0
         self.items = []
         self.price_db = dict(price_db)
+
+    def scan(self, item_name, *args):
+        item_price = self.price_db[item_name]
+        if args:
+            weight = args[0]
+            item = ByWeightItem(item_name, item_price, weight)
+        else:
+            item = ByEachItem(item_name, item_price)
+        self.items.append(item)
+
+    @property
+    def total(self):
+        total = 0
+        for item in self.items:
+            total += item.price
+        return total
--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,27 +1,68 @@
 import pytest
 
+from src.item import Item
 from src.transaction import Transaction
 
 
 @pytest.fixture
 def get_test_db():
-    return {"14oz soup": 1.89, '80% ground beef': 5.99, 'Bananas': 2.38}
-
-def test_a_new_transaction_has_zero_total():
-    trans = Transaction()
-    assert trans.total == 0
+    return {"14oz soup": 1.89, '80% ground beef': 5.99, 'Bananas': 2.38, 't-bone steak': 9.99}
 
 
-def test_a_new_transaction_has_zero_items():
-    trans = Transaction()
-    assert len(trans.items) == 0
+@pytest.fixture
+def get_transaction_with_test_db(get_test_db):
+    return Transaction(get_test_db)
 
 
-def test_a_transaction_has_a_price_db():
-    trans = Transaction()
-    assert trans.price_db is not None
+class TestWiring:
+
+    def test_a_new_transaction_has_zero_total(self):
+        trans = Transaction()
+        assert trans.total == 0
+
+    def test_a_new_transaction_has_zero_items(self):
+        trans = Transaction()
+        assert len(trans.items) == 0
+
+    def test_a_transaction_has_a_price_db(self):
+        trans = Transaction()
+        assert trans.price_db is not None
+
+    def test_can_instantiate_with_price_db(self, get_test_db):
+        trans = Transaction(get_test_db)
+        assert trans.price_db == get_test_db
 
 
-def test_can_instantiate_with_price_db(get_test_db):
-    trans = Transaction(get_test_db)
-    assert trans.price_db == get_test_db
+class TestScanningIndividualItems:
+    def test_can_scan_item(self, get_test_db):
+        trans = Transaction(get_test_db)
+        expected = Item('14oz soup', 1.89)
+
+        trans.scan('14oz soup')
+
+        assert trans.items[0] == expected
+
+    def test_scanning_an_item_increases_transaction_total(self, get_transaction_with_test_db):
+        trans = get_transaction_with_test_db
+
+        trans.scan("14oz soup")
+
+        assert trans.total == 1.89
+
+    def test_adding_multiple_items_increases_cost_with_each_item(self, get_test_db, get_transaction_with_test_db):
+        price_db = get_test_db
+        trans = get_transaction_with_test_db
+
+        trans.scan('14oz soup')
+        assert trans.total == price_db['14oz soup']
+        trans.scan('t-bone steak')
+        assert trans.total == price_db['14oz soup'] + price_db['t-bone steak']
+
+
+class TestByWeightItemScanning:
+    def test_can_scan_item_with_weight(self, get_transaction_with_test_db, get_test_db):
+        trans = get_transaction_with_test_db
+
+        trans.scan('80% ground beef', 2.5)
+
+        assert pytest.approx(trans.total, get_test_db['80% ground beef'] * 2.5)
