e6172703b7604d26bf38d2b80854d87dcc5e5b12
Jon Bos <bos.jon@gmail.com>
2019-10-29 17:46:47 -0500

    Adds ByWeight and ByUnit item classes and tests

--- src/item.py
+++ src/item.py
@@ -1,2 +1,26 @@
 class Item:
-    pass
+    def __init__(self, name, price_per_unit):
+        self.name = name
+        self.price_per_unit = price_per_unit
+
+    @property
+    def price(self):
+        return self.price_per_unit
+
+    def __eq__(self, other):
+        return self.price_per_unit == other.price_per_unit and self.name == other.name
+
+
+class ByEachItem(Item):
+    def __init__(self, name, price_per_unit):
+        super(ByEachItem, self).__init__(name, price_per_unit)
+
+
+class ByWeightItem(Item):
+    def __init__(self, name, price_per_unit, qty):
+        super(ByWeightItem, self).__init__(name, price_per_unit)
+        self.qty = qty
+
+    @property
+    def price(self):
+        return self.price_per_unit * self.qty
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,29 @@
+import pytest
 
+from src.item import ByEachItem, ByWeightItem
+
+
+class TestByEachItem:
+    def test_should_create_item_with_name_and_price(self):
+        milk = ByEachItem('gallon of milk', 3.98)
+
+        assert milk.name == 'gallon of milk'
+        assert milk.price_per_unit == 3.98
+
+    def test_items_are_equal_when_they_have_the_same_name_and_price(self):
+        milk = ByEachItem('gallon of milk', 3.98)
+        other_milk = ByEachItem('gallon of milk', 3.98)
+        heavy_cream = ByEachItem('quart of heavy cream', 5.99)
+
+        assert milk == other_milk
+        assert milk != heavy_cream
+
+
+class TestByWeightItem:
+    def test_by_weight_items_have_qty(self):
+        bulk_chocolate = ByWeightItem('bulk chocolate', 7.99, 3)
+        assert bulk_chocolate.qty == 3
+
+    def test_by_weight_items_price_calculation(self):
+        olive_oil = ByWeightItem('olive oil', 2.99, 10)
+        assert pytest.approx(olive_oil.price, 29.99)
