b0d70427cf8feaf6aea18dc8270d7fc5e41e983b
Jon Bos <bos.jon@gmail.com>
2019-10-29 14:39:42 -0500

    Transaction initializes with price database, adds Item class stub

--- src/transaction.py
+++ src/transaction.py
@@ -1,3 +1,7 @@
 class Transaction:
-    def __init__(self):
+    def __init__(self, price_db=None):
+        if price_db is None:
+            price_db = {}
         self.total = 0
+        self.items = []
+        self.price_db = dict(price_db)
--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,6 +1,27 @@
+import pytest
+
 from src.transaction import Transaction
 
+
+@pytest.fixture
+def get_test_db():
+    return {"14oz soup": 1.89, '80% ground beef': 5.99, 'Bananas': 2.38}
 
 def test_a_new_transaction_has_zero_total():
     trans = Transaction()
     assert trans.total == 0
+
+
+def test_a_new_transaction_has_zero_items():
+    trans = Transaction()
+    assert len(trans.items) == 0
+
+
+def test_a_transaction_has_a_price_db():
+    trans = Transaction()
+    assert trans.price_db is not None
+
+
+def test_can_instantiate_with_price_db(get_test_db):
+    trans = Transaction(get_test_db)
+    assert trans.price_db == get_test_db
