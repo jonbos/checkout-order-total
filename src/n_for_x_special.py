f2c80ab38ff0bcf4f83db52cc5d8dd21487c4c30
Jon Bos <bos.jon@gmail.com>
2019-11-11 16:21:32 -0600

    test: limit support for n for x specials

--- src/n_for_x_special.py
+++ src/n_for_x_special.py
@@ -2,12 +2,17 @@ from special import Special
 
 
 class NforXSpecial(Special):
-    def __init__(self, item_name, special_num, special_price):
+    def __init__(self, item_name, special_num, special_price, limit=None):
         super(NforXSpecial, self).__init__(item_name=item_name)
         self.special_num = special_num
         self.special_price = special_price
+        self.limit = limit
 
     def calculate_discount_amount(self, items):
+        if self.limit:
+            items = items[:self.limit]
         item = items[0]
+        total_units = sum([item.units for item in items])
+        num_discounts = total_units // self.special_num
         actual_cost = item.price_per_unit * self.special_num
-        return abs(actual_cost - self.special_price)
+        return abs(actual_cost - self.special_price) * num_discounts
--- tests/test_n_for_x_special.py
+++ tests/test_n_for_x_special.py
@@ -17,3 +17,10 @@ def test_4_for_3_weighted_item():
     four_for_three = NforXSpecial('one_dollar_item', 4, 3)
     discount_amount = four_for_three.calculate_discount_amount(items)
     assert discount_amount == 1
+
+
+def test_three_for_two_limit_six():
+    items = [one_dollar_item] * 20
+    two_for_three_limit_six = NforXSpecial('one dollar item', 3, 2, 6)
+    discount_amt = two_for_three_limit_six.calculate_discount_amount(items)
+    assert discount_amt == 2
