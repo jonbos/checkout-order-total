879ab18a558c05e1819dd9b05693401965687eec
Jon Bos <bos.jon@gmail.com>
2019-11-12 13:16:38 -0600

    refactor: improved polymorphic constructor for special (i think)

--- src/special.py
+++ src/special.py
@@ -1,8 +1,7 @@
 class Special:
-    def __init__(self, item_name, limit=None):
+    def __init__(self, item_name, limit):
         self.item_name = item_name
         self.limit = limit
 
-    @staticmethod
     def calculate_discount_amount(self, items):
         raise NotImplementedError
